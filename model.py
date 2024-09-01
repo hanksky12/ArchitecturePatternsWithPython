c62669a00d9f8e12f48d13db4849dac5639b2b11
sky <bqt978@gmail.com>
2024-09-01 10:22:43 +0800

    @model chang@
    1.增加註解
    2.完善can_allocate和deallocate

--- model.py
+++ model.py
@@ -9,20 +9,47 @@ class OrderLine:
     sku: str  # 商品名稱
     qty: int  # 數量
 
+    def __hash__(self):
+        return hash(self.orderid)
+
 
 class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date] = None):
+        """
+        一個Batch代表 單一商品的一批庫存
+        """
         self._ref = ref
         self._sku = sku
-        self._available_quantity = qty
+        self._purchased_quantity = qty
         self._eta = eta
+        self._allocations = set()
 
     def allocate(self, line: OrderLine):
-        self._available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
 
     def can_allocate(self, line: OrderLine):
-        return self._sku == line.sku and self._available_quantity >= line.qty
+        # 先檢查sku是否相同，再檢查可用量是否足夠
+        return self._sku == line.sku and self.available_quantity >= line.qty
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other._ref == self._ref
+
+    def __hash__(self):
+        return hash(self._ref)
 
     @property
     def available_quantity(self):
-        return self._available_quantity
+        # 可用量 = 購買量 - 已分配量
+        return self._purchased_quantity - self.allocated_quantity
+
+    @property
+    def allocated_quantity(self):
+        # 已分配量 = 分配給OrderLine的數量總和
+        return sum(line.qty for line in self._allocations)
